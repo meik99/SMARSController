ec8bfb5af64a9ece02d7cfe7364a91356aa54f17
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2020-12-16 12:31:45 +0100

    try sending data via DataItems

--- CoffeeApp/app/src/main/java/com/rynkbit/coffeetogo/android/app/logic/settings/Credentials.kt
+++ CoffeeApp/app/src/main/java/com/rynkbit/coffeetogo/android/app/logic/settings/Credentials.kt
@@ -1,7 +1,15 @@
 package com.rynkbit.coffeetogo.android.app.logic.settings
 
 import android.content.Context
+import android.util.Log
 import androidx.preference.PreferenceManager
+import com.google.android.gms.wearable.DataClient
+import com.google.android.gms.wearable.PutDataMapRequest
+import com.google.android.gms.wearable.Wearable
+import com.google.android.play.core.tasks.Tasks
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class Credentials {
     companion object {
@@ -17,5 +25,18 @@ class Credentials {
     fun getMessagingPassword(context: Context): String {
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
         return sharedPreferences.getString(messagingPasswordKey, "") ?: ""
+    }
+
+    fun putDataToWatch(context: Context, viewModelScope: CoroutineScope) {
+        val putDataRequest = PutDataMapRequest.create("/coffee-messaging-credentials").run {
+            dataMap.putString(messagingUserKey, getMessagingUser(context))
+            dataMap.putString(messagingPasswordKey, getMessagingPassword(context))
+            setUrgent()
+            asPutDataRequest()
+        }
+        val task = Wearable.getDataClient(context).putDataItem(putDataRequest)
+        task.addOnCompleteListener {
+            Log.i(Credentials::class.java.simpleName, "Task completed")
+        }
     }
 }
--- CoffeeApp/app/src/main/java/com/rynkbit/coffeetogo/android/app/ui/main/MainFragment.kt
+++ CoffeeApp/app/src/main/java/com/rynkbit/coffeetogo/android/app/ui/main/MainFragment.kt
@@ -2,6 +2,7 @@ package com.rynkbit.coffeetogo.android.app.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -52,6 +53,8 @@ class MainFragment : Fragment() {
         if (viewModel.messagingUser.value?.isNotEmpty() == true &&
             viewModel.messagingPassword.value?.isNotEmpty() == true) {
             createMakeCoffeeButton()
+            Credentials().putDataToWatch(requireContext(), viewModel.viewModelScope)
+            Log.i(MainFragment::class.java.simpleName, "Synchronized data")
         } else {
             createSetCredentialsButton()
         }
